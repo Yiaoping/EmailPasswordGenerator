66b61a94fbe7cbf826a36fc3a1a1614fb2f33231
Yiaoping <yiao2874@gmail.com>
2019-05-10 22:34:36 -0700

    Error handling

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,33 @@ public class Main {
 		
 		System.out.println("Please enter in a option: \n1) Email \n2) Password \n3) Email + Password \n4) Exit");
 		Scanner sc = new Scanner(System.in);
-		int userOption = Integer.parseInt(sc.nextLine());
-		
-		
-		if(userOption == 1) {
-			System.out.println("Type in any username: ");
-			String user = sc.nextLine();
-			generateEmail(user);
-		}else if (userOption == 2) {
-			System.out.println("Creating password");
-			generatePass();
-		}else if (userOption == 3) {
-			System.out.println("Type in a username: ");
-			String userAndPass = sc.nextLine();
-			generateEmailPassword(userAndPass);
+		try {
+			int userOption = Integer.parseInt(sc.nextLine());
+			if(userOption == 1) {
+				System.out.println("Type in any username: ");
+				String user = sc.nextLine();
+				generateEmail(user);
+			}else if (userOption == 2) {
+				System.out.println("Creating password");
+				generatePass();
+			}else if (userOption == 3) {
+				System.out.println("Type in a username: ");
+				String userAndPass = sc.nextLine();
+				generateEmailPassword(userAndPass);
+			}else {
+				System.out.println("Enter in a digit only, try again");
+				return;
+			}
+		}catch(Exception e){
+			System.out.println("Error occured, try again.");
 		}
-		
+		finally {
+			sc.close();
+		}
+
 	}
 
 
-
-	private static void generatePass() {
-		
-	}
 
 
 
@@ -36,6 +40,12 @@ public class Main {
 		
 		
 	}
+	
+
+	private static void generatePass() {
+		
+	}
+
 	
 	private static void generateEmailPassword(String userAndPass) {
 		
